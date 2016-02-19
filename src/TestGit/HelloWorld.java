119283232a342246de9f9fb81a9cfcf54cede0e5
Alexandr1969 <klymenko2012@meta.ua>
2016-02-19 18:07:38 +0200

    Первое сохранение программы.

---
+++ src/TestGit/HelloWorld.java
@@ -0,0 +1,10 @@
+package TestGit;
+
+/**
+ * Created by Administrator on 19.02.2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
