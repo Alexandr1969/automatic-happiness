8d00e3a0846b05c930bd44140cd0cdeafc2b3f69
Alexandr1969 <klymenko2012@meta.ua>
2016-02-19 18:17:52 +0200

    Второе сохр. пр.

--- src/TestGit/HelloWorld.java
+++ src/TestGit/HelloWorld.java
@@ -6,5 +6,6 @@ package TestGit;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Add more texts");
     }
 }
